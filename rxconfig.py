22c359598f5b14810089632e1ba70f5328c3f663
Cristian Ahumada <ahumadacristian79@gmail.com>
2024-11-10 18:46:28 -0300

    version 0.3
    union back y frontend

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,9 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Projects"
+    app_name="Projects",
+    cors_allowed_origins=[
+        "http://localhost:3000"
+        "https://web-beige-omega.vercel.app"
+    ]
 )
